d215d2c8bcc76bc6b8fb3a769c5bb34defaf85fb
Ruben Torres Frias <isctorres@gmail.com>
2024-08-01 12:04:05 -0600

    ultimos cambios 2

--- lib/screens/intents_screen.dart
+++ lib/screens/intents_screen.dart
@@ -7,14 +7,26 @@ class IntentsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView(
+        padding: EdgeInsets.all(10),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2
-        )
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10
+        ),
+        children: [
+          intentCard(title: 'https://itcelaya.edu.mx',color: Colors.green,image: 'assets/logo.png'),
+          intentCard(title: 'tel:4612279093',color: Colors.blueAccent,image: 'assets/logo.png'),
+          intentCard(title: 'sms:4612279093',color: Colors.orange,image: 'assets/logo.png'),
+          intentCard(title: 'ruben.torres@itcelaya.edu.mx',color: Colors.purpleAccent,image: 'assets/logo.png'),
+        ],
       ),
     );
   }
 
-  Widget intentCard(String title, String image, Color color){
+  Widget intentCard({
+    required String title, 
+    required String image, 
+    required Color color}){
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
@@ -22,7 +34,7 @@ class IntentsScreen extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Image.asset(image),
+          Image.asset(image,height: 130,),
           Text(title)
         ],
       ),
